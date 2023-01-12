761391d49e11341f12820df55f0316199f64056c
Lisa Jenkins <lisa@devpipline.com>
2023-01-12 14:32:21 -0700

    add total cart function

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -10,11 +10,20 @@ export default function Cart() {
       return <CartItem key={item.id} item={item} />;
     });
   }
+  function renderTotal() {
+    if (cart.length > 0) {
+      return cart.reduce((prev, curr) => prev + curr.price, 0);
+    }
+    return false;
+  }
 
   return (
     <div className="cart-content">
       <h1>Cart</h1>
       {renderCart()}
+      <div className="totalCart">
+        <h1>Your Total is</h1> {renderTotal() || "0"}
+      </div>
       <div className="store-clear-cart">
         <button onClick={clearCart}>Clear Cart</button>
       </div>
